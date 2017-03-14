3ad53db4702601b3882446a3350698930ecaee2e
Pramod Anarase <pramodanarase@gmail.com>
2017-03-14 18:33:55 +0530

    Added login details

--- angular2-demo/src/app/login/login.component.ts
+++ angular2-demo/src/app/login/login.component.ts
@@ -4,9 +4,10 @@ import { Component } from '@angular/core';
 
 
 @Component({
-      templateUrl: 'login.component.html'
+      templateUrl: 'login.component.html',
+      styleUrls : ['login.css']      
 })
 
 export class LoginComponent {
-
+ 
 }
